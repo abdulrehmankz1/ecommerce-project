825f1e38da2d84ee9ba36ff4c68cea90b99aef4f
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-01 19:44:12 +0500

    add navbar and pages

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import Image from 'next/image';
+import logo from '../../../public/assets/images/logo.svg';
+// import '../../styles/globals.scss';
 
-function Navbar() {
+
+
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="container mx-auto pt-8 ">
 
-export default Navbar
+    <div className="w-full inline-flex pb-6">
+    <i className="fa-solid fa-magnifying-glass text-xl"></i>
+      <div className="flex items-center justify-center" style={{position: "relative", left: "42%"}}>
+      <Image src={logo} alt="" height="100" width="180" className='mx-auto' />
+      </div>
+      <div className="flex items-center ms-auto">
+        <button className="flex items-center justify-center hover:text-red-500 transition-colors duration-300 ease-in-out">
+        <i className="fa-solid fa-user mr-2 text-xl"></i>
+          <p className="font-roboto font-normal text-lg">Account</p>
+        </button>
+        <button className="flex items-center justify-center ml-4 hover:text-red-500 transition-colors duration-300 ease-in-out" 
+        >
+        <i className="fa-solid fa-bag-shopping mr-2 text-xl"></i>
+          <p  className="font-roboto font-normal text-lg">Shopping</p>
+        </button>
+      </div>
+    </div>
+    <hr />
+
+    <div className='pt-7'>
+    <ul className="flex justify-between flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                <li>
+                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out"  aria-current="page">Jewelry & Accessories</a>
+                </li>
+                <li>
+                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Clothing & Shoes</a>
+                </li>
+                <li>
+                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Wedding & Party</a>
+                </li>
+                <li>
+                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Toys & Entertainment</a>
+                </li>
+                <li>
+                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Art & Collectibles</a>
+                </li>
+                <li>
+                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Craft Supplies & Tools</a>
+                </li>
+            </ul>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import '../styles/globals.scss';
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import ProductSection from "./components/ProductSection";
+
 
 export default function Home() {
   return (
     <div>
   <Navbar />
+  <HeroSection />
+  <ProductSection />
     </div>
   );
 }
