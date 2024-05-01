5d26516908d5731ffe98966549dba6f5774fbc03
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-01 13:57:30 +0500

    add icons and navbar files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import '../styles/globals.scss';
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-  
+    <div>
+  <Navbar />
     </div>
   );
 }
