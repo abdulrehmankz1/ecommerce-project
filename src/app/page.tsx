b27fb8556c03f5185c2c8d668d02232279b6260e
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-01 13:20:55 +0500

    project init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import '../styles/globals.scss';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+  
+    </div>
+  );
+}
