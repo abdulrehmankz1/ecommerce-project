86294eb702403856b07a87e4c006be6c4e6f3329
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-02 19:05:38 +0500

    aadd navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,37 +15,37 @@ const Navbar = () => {
       </div>
       <div className="flex items-center ms-auto">
         <button className="flex items-center justify-center hover:text-red-500 transition-colors duration-300 ease-in-out">
-        <i className="fa-solid fa-user mr-2 text-xl"></i>
-          <p className="font-roboto font-normal text-lg">Account</p>
+        <i className="fa-solid fa-user mr-2 text-xl text-[#072b4b]"></i>
+          <p className="font-roboto font-normal text-lg text-[#072b4b]">Account</p>
         </button>
         <button className="flex items-center justify-center ml-4 hover:text-red-500 transition-colors duration-300 ease-in-out" 
         >
-        <i className="fa-solid fa-bag-shopping mr-2 text-xl"></i>
-          <p  className="font-roboto font-normal text-lg">Shopping</p>
+        <i className="fa-solid fa-bag-shopping mr-2 text-xl text-[#072b4b]"></i>
+          <p  className="font-roboto font-normal text-lg text-[#072b4b]">Shopping</p>
         </button>
       </div>
     </div>
     <hr />
 
     <div className='pt-7'>
-    <ul className="flex justify-between flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+    <ul className="flex justify-between flex-row font-base mt-0 space-x-8 rtl:space-x-reverse text-sm font-sans text-[#000000]">
                 <li>
-                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out"  aria-current="page">Jewelry & Accessories</a>
+                    <a href="#" className=" hover:text-red-500 transition-colors duration-300 ease-in-out"  aria-current="page">Jewelry & Accessories</a>
                 </li>
                 <li>
-                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Clothing & Shoes</a>
+                    <a href="#" className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Clothing & Shoes</a>
                 </li>
                 <li>
-                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Wedding & Party</a>
+                    <a href="#" className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Wedding & Party</a>
                 </li>
                 <li>
-                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Toys & Entertainment</a>
+                    <a href="#" className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Toys & Entertainment</a>
                 </li>
                 <li>
-                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Art & Collectibles</a>
+                    <a href="#" className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Art & Collectibles</a>
                 </li>
                 <li>
-                    <a href="#"   className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Craft Supplies & Tools</a>
+                    <a href="#" className=" hover:text-red-500 transition-colors duration-300 ease-in-out" >Craft Supplies & Tools</a>
                 </li>
             </ul>
     </div>
