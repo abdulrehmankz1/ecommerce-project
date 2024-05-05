e10705aab6866869e32cbbcbecfb30e25c171063
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-05 15:08:25 +0500

    add client.tsx with api

---
+++ src/lib/client.tsx
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
