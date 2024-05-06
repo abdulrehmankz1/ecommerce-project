80aac81c9ae002085d22b64a4ecb16fb9bc9ab09
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-06 13:49:51 +0500

    add isActive functionality

--- src/app/components/ProductSection.tsx
+++ src/app/components/ProductSection.tsx
@@ -12,7 +12,7 @@ import jacket from "../../../public/assets/images/jacket.svg";
 
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const GET_TAGS = gql`
   query {
@@ -28,11 +28,23 @@ const GET_TAGS = gql`
 
 const ProductSection = () => {
   const { loading, error, data } = useQuery(GET_TAGS);
-  const router = useRouter();
-  if (loading) return <p>Loading menu items...</p>;
+  const searchParams = useSearchParams()
+
+  if (loading) return (
+    <div className="flex items-center justify-center container mx-auto border border-gray-200 rounded-lg bg-gray-50 h-40">
+      <div role="status">
+        <svg aria-hidden="true" className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
   if (error) return <p>Error loading menu items: {error.message}</p>;
 
+
+  const search = searchParams.get('tag');
+
   console.log("data", data);
+
 
   return (
     <div className="container mx-auto">
@@ -44,24 +56,28 @@ const ProductSection = () => {
               <ul className="flex">
                 <li className="mr-5">
                   <Link
-                    href="#"
-                    className="hover:text-red-500 transition-colors duration-300 ease-in-out font-semibold"
+                    href="/"
+                    className={`${!search ? "font-bold" : ""
+                      } hover:text-red-500 transition-colors duration-300 ease-in-out`}
                   >
                     All Products
                   </Link>
                 </li>
-                {data.tags.nodes.map((menuItem: any) => (
-                  <li key={menuItem.id} className="mr-5">
-                    <Link
-                      href={{
-                        query: { tag: menuItem.displayTitle.toLowerCase() },
-                      }}
-                      className="hover:text-red-500 transition-colors duration-300 ease-in-out text-[#808080] font-sans"
-                    >
-                      {menuItem.displayTitle}
-                    </Link>
-                  </li>
-                ))}
+                {data.tags.nodes.map((menuItem: any) => {
+                  const isActive = menuItem.slug === search || (!search && menuItem.name === "All Products");
+                  return (
+                    <li key={menuItem.id} className="mr-5">
+                      <Link
+                        href={{
+                          query: { tag: menuItem.slug },
+                        }}
+                        className={`${isActive ? "font-bold text-black" : ""}  hover:text-red-500 transition-colors duration-300 ease-in-out text-[#808080] font-sans`}
+                      >
+                        {menuItem.displayTitle}
+                      </Link>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
             <button className="text-white py-2 px-4 rounded flex align-middle bg-gray-800 hover:bg-red-500 font-sans">
