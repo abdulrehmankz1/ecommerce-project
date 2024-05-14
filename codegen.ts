603f122e3dbcaf7d830d5490c954a09a1d9e8f0f
abdulrehman1 <khanzadaabdulrehman1@gmail.com>
2024-05-14 13:25:45 +0500

    set up GraphQL Code Generator with TypeScript

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'https://explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com/graphql',
+  documents: ['src/app/**/*.{ts,tsx}'],
+  generates: {
+    './src/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+      }
+    }
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
