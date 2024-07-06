d19bf3b1cde00f668d9a6d0ad4fd252bde919e2f
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-07-06 19:55:47 +0500

    feat: Refactor tag and product fetching logic into hooks

--- src/app/hooks/useGetProducts.tsx
+++ src/app/hooks/useGetProducts.tsx
@@ -1,9 +1,26 @@
 import { useQuery } from '@apollo/client';
-import { GET_PRODUCTS } from '../queries';
+import { GET_PRODUCTS } from '@/app/queries';
 
-export const useGetProducts = (shopIds: string[]) => {
+import dummyImage from "../../../public/assets/images/dummy-image.png";
+
+export const useGetProducts = (shopIds: string[], selectedTagId?: string | null) => {
+  const variables = {
+    shopIds,
+    ...(selectedTagId && { tagIds: [selectedTagId] }),
+  };
+
   const { loading, error, data } = useQuery(GET_PRODUCTS, {
-    variables: { shopIds },
+    variables,
   });
-  return { loading, error, data };
+
+  const products = data?.catalogItems?.edges
+    .filter((edge: any) => edge?.node?.__typename === "CatalogItemProduct" && edge.node.product)
+    .map((edge: any) => {
+      const { slug, title, pricing, primaryImage } = edge.node.product;
+      const imageUrl = primaryImage?.URLs?.original || dummyImage;
+      const href = `/products/${slug}`;
+      return { id: edge.node.product._id, href, title, pricing, imageUrl };
+    }) || [];
+
+  return { loading, error, data, products };
 };
--- src/app/hooks/useGetTags.tsx
+++ src/app/hooks/useGetTags.tsx
@@ -1,9 +1,13 @@
 import { useQuery } from '@apollo/client';
-import { GET_TAGS } from '../queries';
+
+import { GET_TAGS } from '@/app/queries';
+import { Tag } from '@/app/components/types';
 
 export const useGetTags = (shopId: string) => {
   const { loading, error, data } = useQuery(GET_TAGS, {
     variables: { shopId },
   });
-  return { loading, error, data };
+
+  const menuItems: Tag[] = data?.tags?.nodes || [];
+  return { loading, error, data, menuItems };
 };
