4f85a956a2f6b18c0b0bd40dd9d8cff0fa55ae51
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-07-10 17:32:18 +0500

    remove suspense rule

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ const nextConfig = {
   images: {
     domains: ['explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com'],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
